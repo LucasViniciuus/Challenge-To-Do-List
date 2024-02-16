9186e7a2008f6b82e6ddb237bae2c4b4f98d17b9
Lucas Vinicius <lucas.vinicius03@outlook.com>
2024-02-16 02:46:49 -0300

    Added the task repository to perform new queries and use JPA.

---
+++ src/main/java/com/todolist/repositories/TaskRepository.java
@@ -0,0 +1,8 @@
+package com.todolist.repositories;
+
+import com.todolist.entities.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface TaskRepository extends JpaRepository<Task, Integer> {
+}
